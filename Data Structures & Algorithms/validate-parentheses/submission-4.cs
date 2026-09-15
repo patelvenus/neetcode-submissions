62c2e045c195333c9a3b084ff19f298de057560f
patelvenus <venuspatel30@gmail.com>
2026-09-15 15:58:28 -0700

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public bool IsValid(string s) {
+        
+        var stack = new Stack<char>();
+
+        foreach(char c in s)
+        {
+            if( c == '(' || c == '{' || c == '[' )
+                stack.Push(c);
+            else
+            {
+                if(stack.Count <= 0)
+                    return false;
+                
+                var p = stack.Peek();
+                
+                if(c == ']' && p!= '[') return false;
+                else if(c == '}' && p!= '{') return false;
+                else if(c == ')' && p!= '(') return false;
+                stack.Pop();
+            }
+        }
+        return stack.Count == 0;
+    }
+}

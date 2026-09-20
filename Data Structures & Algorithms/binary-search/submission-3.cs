a2e0a93353ac9f489254360c301b859ca73a742a
patelvenus <venuspatel30@gmail.com>
2026-09-20 11:25:26 -0700

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        return BinarySearch(nums, 0, nums.Length - 1, target);
+    }
+
+    // time : (NLogN)
+    // space : (NLogN)
+    // public int BinarySearch(int[] nums, int start, int end, int target)
+    // {
+    //     if(start > end)
+    //         return -1;
+        
+    //     int mid = (start + end) /2;
+        
+    //     if(nums[mid] == target)
+    //         return mid;
+        
+    //     if(nums[mid] > target)
+    //         return BinarySearch(nums, start, mid-1, target);
+    //     else
+    //         return BinarySearch(nums, mid+1, end, target);
+    // }
+
+    public int BinarySearch(int[] nums, int start, int end, int target)
+    {
+        while(start <= end)
+        {
+            int mid = (start + end) /2;
+        
+            if(nums[mid] == target)
+                return mid;
+
+            if(nums[mid] > target)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+
+        return -1;
+    }
+}
